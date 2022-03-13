a3b85c4ff576cec49648c9da98a2a1083d58745f
Weicong Kong <weicong.kong@gmail.com>
2022-03-13 21:17:28 +1100

    add the kaggle feadback metric computation, the most efficient code so far

--- debug-ensemble.py
+++ debug-ensemble.py
@@ -307,8 +307,6 @@ min_thresh = {
     "Rebuttal": 4,
 }
 
-submission = []
-
 
 def build_feedback_prediction_string(
         preds, sample_pred_scores, sample_id, sample_text, offset_mapping):
@@ -357,6 +355,8 @@ def build_feedback_prediction_string(
     return temp_df
 
 
+submission = []
+
 for sample_idx, sample in enumerate(train_debug_samples):
     preds = sample["preds"]
     offset_mapping = sample["offset_mapping"]
@@ -370,3 +370,81 @@ for sample_idx, sample in enumerate(train_debug_samples):
 
 submission = pd.concat(submission).reset_index(drop=True)
 submission = link_evidence(submission)
+
+train_pref_df = submission.copy()
+train_groud_truth_df = train_df[train_df['id'].isin(train_pref_df['id'].unique())].copy()
+
+
+# the following 3 funcs are from https://www.kaggle.com/cpmpml/faster-metric-computation
+def calc_overlap3(set_pred, set_gt):
+    """
+    Calculates if the overlap between prediction and
+    ground truth is enough fora potential True positive
+    """
+    # Length of each and intersection
+    try:
+        len_gt = len(set_gt)
+        len_pred = len(set_pred)
+        inter = len(set_gt & set_pred)
+        overlap_1 = inter / len_gt
+        overlap_2 = inter/ len_pred
+        return overlap_1 >= 0.5 and overlap_2 >= 0.5
+    except:  # at least one of the input is NaN
+        return False
+
+
+def score_feedback_comp_micro3(pred_df, gt_df, discourse_type):
+    """
+    A function that scores for the kaggle
+        Student Writing Competition
+
+    Uses the steps in the evaluation page here:
+        https://www.kaggle.com/c/feedback-prize-2021/overview/evaluation
+    """
+    gt_df = gt_df.loc[gt_df['discourse_type'] == discourse_type, ['id', 'predictionstring']].reset_index(drop=True)
+    pred_df = pred_df.loc[pred_df['class'] == discourse_type, ['id', 'predictionstring']].reset_index(drop=True)
+    pred_df['pred_id'] = pred_df.index
+    gt_df['gt_id'] = gt_df.index
+    pred_df['predictionstring'] = [set(pred.split(' ')) for pred in pred_df['predictionstring']]
+    gt_df['predictionstring'] = [set(pred.split(' ')) for pred in gt_df['predictionstring']]
+
+    # Step 1. all ground truths and predictions for a given class are compared.
+    joined = pred_df.merge(gt_df,
+                           left_on='id',
+                           right_on='id',
+                           how='outer',
+                           suffixes=('_pred', '_gt')
+                           )
+    overlaps = [calc_overlap3(*args) for args in zip(joined.predictionstring_pred,
+                                                     joined.predictionstring_gt)]
+
+    # 2. If the overlap between the ground truth and prediction is >= 0.5,
+    # and the overlap between the prediction and the ground truth >= 0.5,
+    # the prediction is a match and considered a true positive.
+    # If multiple matches exist, the match with the highest pair of overlaps is taken.
+    # we don't need to compute the match to compute the score
+    TP = joined.loc[overlaps]['gt_id'].nunique()
+
+    # 3. Any unmatched ground truths are false negatives
+    # and any unmatched predictions are false positives.
+    TPandFP = len(pred_df)
+    TPandFN = len(gt_df)
+
+    # calc microf1
+    my_f1_score = 2 * TP / (TPandFP + TPandFN)
+    return my_f1_score
+
+
+def score_feedback_comp3(pred_df, gt_df, return_class_scores=False):
+    class_scores = {}
+    for discourse_type in gt_df.discourse_type.unique():
+        class_score = score_feedback_comp_micro3(pred_df, gt_df, discourse_type)
+        class_scores[discourse_type] = class_score
+    f1 = np.mean([v for v in class_scores.values()])
+    if return_class_scores:
+        return f1, class_scores
+    return f1
+
+
+micro_lead = score_feedback_comp_micro3(train_pref_df, train_groud_truth_df, 'Lead')
+macro_f1 = score_feedback_comp3(train_pref_df, train_groud_truth_df)
